951a99579890d0f9f5d8c4deabb3e3aafb9c8580
ryzzaki <cuong.nugi@gmail.com>
2020-03-28 23:52:53 +0100

    feat: strapping sass & bulma

--- frontend-sb/src/components/Home.js
+++ frontend-sb/src/components/Home.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export class Home extends React.Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <div className="App section">
+        <h1>Hello World!</h1>
+        <button className="button is-danger is-outlined">Button</button>
+      </div>
+    );
   }
 }
